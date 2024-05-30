ff1031edb924930ffc2f3ca5394e002873364ca5
Serafin Quesada <quesada.serafin03@gmail.com>
2024-05-30 17:51:37 -0300

    fixing responsive styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import MyImage from '@/components/Image'
 export default function Home() {
 	return (
 		<main className=" flex min-h-screen flex-col items-center justify-between  ">
-			<div className="columns-1 gap-4 px-24 pt-6 sm:columns-2 lg:columns-3 lg:gap-6  [&>img:not(:first-child)]:mt-5 lg:[&>img:not(:first-child)]:mt-8">
+			<div className="columns-1 gap-4 px-12 pt-6 md:columns-2 md:px-24  lg:columns-3 lg:gap-6  [&>img:not(:first-child)]:mt-5 lg:[&>img:not(:first-child)]:mt-8">
 				{/* {imgs.map(img => <MyImage key={img.text} src={img.src}  alt={img.text}/>
 				)} */}
 				<MyImage
