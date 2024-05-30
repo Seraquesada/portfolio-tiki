f0b19674c68b4b3d44312c4dbb3e2fe01dd7dc6f
Serafin Quesada <quesada.serafin03@gmail.com>
2024-05-30 15:25:20 -0300

    updating config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        formats: ['image/avif', 'image/webp'],
-    }
-};
+import withPlaiceholder from '@plaiceholder/next'
 
-export default nextConfig;
+const nextConfig = {
+	images: {
+		formats: ['image/avif', 'image/webp'],
+	}
+}
+
+export default withPlaiceholder(nextConfig)
